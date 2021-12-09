4b9fb4753156af997dbb96dc174bf547c881af34
Andrea <andreagf91@gmail.com>
2021-12-09 13:57:31 +0100

    01. Creada estructura, los dos atributos y el constructor de la clase Bateria.

---
+++ Bateria.java
@@ -0,0 +1,18 @@
+/**
+ * @author (Alejandro Fernandez y Andrea Gonzalez) 
+ * @version (09/12/2021)
+ */
+public class Bateria {
+    // Capacidad de la bateria.
+    private int capacidad;
+    // Estado de la bateria.
+    private boolean cargada;
+
+    /**
+     * Crea una Bateria
+     */
+    public Bateria(int mAh, boolean charge){
+        capacidad  = mAh;
+        charge = cargada;
+    }
+}
