2e2a9d23fa7a4025af6f518759c875cb4fb139db
Andrea <andreagf91@gmail.com>
2021-12-15 09:28:15 +0100

    03. Añadido método imprimirDetalles a la clase Bateria.

--- Bateria.java
+++ Bateria.java
@@ -20,7 +20,7 @@ public class Bateria {
         return capacidad;
     }
     
-    public boolean getCharge(){
+    public boolean getCargada(){
         return cargada;
     }
     
@@ -28,7 +28,21 @@ public class Bateria {
         capacidad = laCapacidad;    
     }
     
-    public void setCharge(boolean charged){
-        cargada= charged;
+    public void setCargada(boolean charged){
+        cargada = charged;
+    }
+
+    /**
+     * Imprime los detalles de la batería.
+     */
+    public void imprimirDetalles () {
+        if (cargada) {  
+            System.out.println ("La batería está cargada.");
+        }
+
+        else {
+            System.out.println ("La batería está descargada.");
+        }
+        System.out.println ("Tiene una capacidad de " +  (capacidad)  + " mAh.");
     }
 }
